6d202f770c6fcb51bb4cf9915178dd1e26d01b20
daniel <danielparmstrong1@gmail.com>
2025-03-28 10:51:10 +0100

    changes to prompts and new tools

---
+++ src/steerable_retro/llm/prompts/rewriting_prep.py
@@ -0,0 +1,19 @@
+rewriting_prep = """
+Here is the provided code snippet along with the outputs.
+
+<code_and_description>
+{CODE_AND_DESCRIPTION}
+</code_and_description>
+
+<test_case>
+{TEST_CASE}
+</test_case>
+
+<stdout>
+{stdout}
+</stdout>
+
+<errors>
+{ERRORS}
+</errors>
+"""
